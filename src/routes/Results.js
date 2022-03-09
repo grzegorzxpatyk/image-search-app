9a9cbebc249ec87eec80b401fabe9107af10e8cb
Grzegorz Patyk <grzegorz.p4tyk@gmail.com>
2022-03-09 23:55:33 +0100

    edit route components

---
+++ src/routes/Results.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Results() {
+    return (
+        <div className="h-100 w-100 d-flex justify-content-center align-items-center">
+            Results
+        </div>
+    );
+}
--- src/routes/Search.js
+++ src/routes/Search.js
@@ -5,15 +5,15 @@ function Search() {
     function onEnter(event) {
         if (event.keyCode === 13) {
             event.preventDefault();
-            alert('Enter pressed! input: ' + event.target.value);
+            console.log('Enter pressed! input: ' + event.target.value);
         }
     }
 
     return (
-        <div className="d-flex justify-content-center align-items-center flex-column text-center text-white bg-dark">
+        <>
             <h1>This is app.js</h1>
             <SearchBar onEnter={onEnter} />
-        </div>
+        </>
     );
 }
 
