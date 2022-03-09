7451343b99aa4133cf9be6e596d1c803e7a60d54
Grzegorz Patyk <grzegorz.p4tyk@gmail.com>
2022-03-09 23:54:53 +0100

    add navbar for development and Outlet component to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { NavLink, Outlet } from 'react-router-dom';
 import './App.scss';
-import Search from './routes/Search';
+
 function App() {
     return (
-        <>
-            <Search />
-        </>
+        <div className="d-flex justify-content-center align-items-center flex-column text-center text-white bg-dark h-100">
+            <nav className="d-flex flex-row justify-content-evenly w-100 fixed-top">
+                <NavLink to="/">Search</NavLink>
+                <NavLink to="/results">Results</NavLink>
+            </nav>
+            <Outlet />
+        </div>
     );
 }
 
