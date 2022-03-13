b5ed888899f7f5d2db1d8a8df85f6648ab4f7940
Grzegorz Patyk <grzegorz.p4tyk@gmail.com>
2022-03-13 01:54:13 +0100

    finally implemented the proper query parsing and cleanup

--- src/App.js
+++ src/App.js
@@ -4,53 +4,47 @@ import {
     Outlet,
     useSearchParams,
     useNavigate,
-    // useLocation,
+    useLocation,
+    createSearchParams,
 } from 'react-router-dom';
 import './App.scss';
 
 function App() {
     let [searchParams, setSearchParams] = useSearchParams();
     let navigate = useNavigate();
-    // let location = useLocation();
+    let location = useLocation();
     let [imgs, setImgs] = useState([]);
     let [query, setQuery] = useState();
 
-    // useEffect(() => {
-    //     const currentParams = Object.fromEntries([...searchParams]);
-    //     console.log(currentParams); // get new values onchange
-    // }, [searchParams]);
-
-    async function onEnter(event) {
-        // if (event.keyCode === 13) {
+    function onEnter(event) {
         event.preventDefault();
 
         const apiRoot = process.env.REACT_APP_APIROOT;
         const accessKey = process.env.REACT_APP_ACCESSKEY;
 
         setImgs(() => []);
-        // navigate('/results');
         if (query) {
-            navigate({ pathname: '/results', search: `?query=${query}` }); // , search: `?query=${query}`
-            // setSearchParams({ query });
+            if (location.pathname === '/') {
+                navigate({
+                    pathname: 'results',
+                    search: `?${createSearchParams({ query: query })}`,
+                });
+            } else if (location.pathname === '/results') {
+                setSearchParams({ query });
+            }
             console.log(query);
             console.log('searchParams ' + searchParams.get('query'));
-            // navigate('/results' + location.search);
-            // navigate(`/results?query=${searchParams.get('query')}`);
-            // location.search = `?query=${query}`;
         } else {
             setSearchParams({});
             navigate({ search: `` });
             console.log('else');
         }
 
-        // navigate('/results' + location.search);
-
         axios
             .get(
                 `${apiRoot}/search/photos?client_id=${accessKey}&query=${query}&count=10`
             )
             .then((response) => {
-                // console.log(response);
                 response.data.results.forEach((result) => {
                     setImgs((prevImgs) => [
                         ...prevImgs,
@@ -61,7 +55,6 @@ function App() {
                     ]);
                 });
             });
-        // }
     }
 
     return (
@@ -73,7 +66,16 @@ function App() {
                 <NavLink to="/">Search</NavLink>
                 <NavLink to="/results">Results</NavLink>
             </nav> */}
-            <Outlet context={[imgs, setImgs, onEnter, query, setQuery]} />
+            <Outlet
+                context={[
+                    imgs,
+                    setImgs,
+                    onEnter,
+                    query,
+                    setQuery,
+                    searchParams,
+                ]}
+            />
         </div>
     );
 }
