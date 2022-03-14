db85dc0278ee2022635dbd65f29345409e8036d3
Grzegorz Patyk <grzegorz.p4tyk@gmail.com>
2022-03-14 21:14:52 +0100

    add fields to API response mapping

--- src/routes/Results.js
+++ src/routes/Results.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useOutletContext } from 'react-router-dom';
 import SearchBar from '../components/SearchBar';
 import UnsplashImage from '../components/UnsplashImage';
+import ModalImage from '../components/ModalImage';
 import './Results.scss';
 
 export default function Results() {
     const [imgs, setImgs, onEnter, query, setQuery, searchParams] =
         useOutletContext();
+    let [modalShow, setModalShow] = useState({});
+    useEffect(() => {
+        imgs.forEach((img) => {
+            setModalShow((prev) => ({ ...prev, [`Modal_${img.id}`]: false }));
+        });
+        return () => {
+            setModalShow({});
+        };
+    }, [imgs]);
 
     return (
         <div className="h-100 w-100 d-flex justify-content-center align-items-center flex-column overflow-auto">
@@ -14,7 +24,29 @@ export default function Results() {
             <h1>{searchParams.get('query')}</h1>
             <div className="imgs">
                 {imgs.map((img) => (
-                    <UnsplashImage {...img} key={img.id} />
+                    <>
+                        <UnsplashImage
+                            {...img}
+                            key={img.id}
+                            onClick={() =>
+                                setModalShow((prev) => ({
+                                    ...prev,
+                                    [`M_${img.id}`]: true,
+                                }))
+                            }
+                        />
+                        <ModalImage
+                            key={`M_${img.id}`}
+                            show={modalShow[`M_${img.id}`]}
+                            onHide={() =>
+                                setModalShow((prev) => ({
+                                    ...prev,
+                                    [`M_${img.id}`]: false,
+                                }))
+                            }
+                            {...img}
+                        />
+                    </>
                 ))}
             </div>
         </div>
