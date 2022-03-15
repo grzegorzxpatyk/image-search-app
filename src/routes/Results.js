3860260cf9a9a3a342781932d1dfd294096d44db
Grzegorz Patyk <grzegorz.p4tyk@gmail.com>
2022-03-15 01:28:15 +0100

    add app logo and fix searchBar styling

--- src/routes/Results.js
+++ src/routes/Results.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useOutletContext } from 'react-router-dom';
+import { NavLink, useOutletContext } from 'react-router-dom';
 import SearchBar from '../components/SearchBar';
 import UnsplashImage from '../components/UnsplashImage';
 import ModalImage from '../components/ModalImage';
@@ -11,7 +11,7 @@ export default function Results() {
     let [modalShow, setModalShow] = useState({});
     useEffect(() => {
         imgs.forEach((img) => {
-            setModalShow((prev) => ({ ...prev, [`Modal_${img.id}`]: false }));
+            setModalShow((prev) => ({ ...prev, [`M_${img.id}`]: false }));
         });
         return () => {
             setModalShow({});
@@ -19,36 +19,43 @@ export default function Results() {
     }, [imgs]);
 
     return (
-        <div className="h-100 w-100 d-flex justify-content-center align-items-center flex-column overflow-auto">
-            <SearchBar setQuery={setQuery} onEnter={onEnter} />
-            <h1>{searchParams.get('query')}</h1>
-            <div className="imgs">
-                {imgs.map((img) => (
-                    <>
-                        <UnsplashImage
-                            {...img}
-                            key={img.id}
-                            onClick={() =>
-                                setModalShow((prev) => ({
-                                    ...prev,
-                                    [`M_${img.id}`]: true,
-                                }))
-                            }
-                        />
-                        <ModalImage
-                            key={`M_${img.id}`}
-                            show={modalShow[`M_${img.id}`]}
-                            onHide={() =>
-                                setModalShow((prev) => ({
-                                    ...prev,
-                                    [`M_${img.id}`]: false,
-                                }))
-                            }
-                            {...img}
-                        />
-                    </>
-                ))}
-            </div>
+        <div className="h-100 w-100 d-flex justify-content-center align-items-center flex-column overflow-auto mt-3">
+            <header className="container mb-5 d-flex flex-column align-items-start justify-content-between">
+                <h4 className="app-logo">
+                    <NavLink to="/">image-search-app</NavLink>
+                </h4>
+                <SearchBar setQuery={setQuery} onEnter={onEnter} />
+                <h1 className="query">{searchParams.get('query')}</h1>
+            </header>
+            <main>
+                <div className="imgs">
+                    {imgs.map((img) => (
+                        <>
+                            <UnsplashImage
+                                {...img}
+                                key={img.id}
+                                onClick={() =>
+                                    setModalShow((prev) => ({
+                                        ...prev,
+                                        [`M_${img.id}`]: true,
+                                    }))
+                                }
+                            />
+                            <ModalImage
+                                key={`M_${img.id}`}
+                                show={modalShow[`M_${img.id}`]}
+                                onHide={() =>
+                                    setModalShow((prev) => ({
+                                        ...prev,
+                                        [`M_${img.id}`]: false,
+                                    }))
+                                }
+                                {...img}
+                            />
+                        </>
+                    ))}
+                </div>
+            </main>
         </div>
     );
 }
