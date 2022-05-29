8bb73c4551d704a11c5500e4343d8dbc4ae709af
Grzegorz Patyk <grzegorz.p4tyk@gmail.com>
2022-05-29 23:16:40 +0200

    add container div to UnsplashImage component

--- src/components/UnsplashImage.js
+++ src/components/UnsplashImage.js
@@ -2,5 +2,10 @@ import React from 'react';
 import './UnsplashImage.scss';
 
 export default function UnsplashImage({ urls, alt, id, onClick }) {
-    return <img key={id} src={urls.regular} alt={alt} onClick={onClick} />;
+    console.log(id);
+    return (
+        <div className="img-container">
+            <img key={id} src={urls.regular} alt={alt} onClick={onClick} />
+        </div>
+    );
 }
